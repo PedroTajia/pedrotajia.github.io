e35e88467d6b810157df9b31921d19eb440e9626
PedroTajia <105690736+PedroTajia@users.noreply.github.com>
2025-12-28 02:35:44 +0000

    Deploying to gh-pages from @ PedroTajia/pedrotajia.github.io@84d1a3ee59adfece9e465f3c222969ad1ffdbaa3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "PedroTajia/tajiapedro.github.io",
+    "data-repo": "PedroTajia/pedrotajia.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
